76e6bb8e7652a85ea06cd9a18e8eebb9e54e5314
Xiaodong <403533049@qq.com>
2020-06-07 22:44:17 +0800

    ship

---
+++ alien_invasion.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import sys
+
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+	pygame.init()
+	ai_settings = Settings()
+	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+	pygame.display.set_caption("Alien Invasion")
+
+	ship = Ship(screen)
+
+
+
+	bg_color = (230,230,230)
+
+	while True:
+		gf.check_events()
+		gf.update_screen(ai_settings,screen,ship)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+		screen.fill(ai_settings.bg_color)
+		ship.blitme()
+
+
+		pygame.display.flip()
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+class Settings():
+	"""docstring for Settings"""
+	def __init__(self):
+		
+		self.screen_width = 1200
+		self.screen_height = 600
+		self.bg_color = (230,230,230)
+		
