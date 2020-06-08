1f892b57c07cd406e64860ba62f3119a96039be4
Xiaodong <403533049@qq.com>
2020-06-08 21:51:58 +0800

    step two

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,20 +14,21 @@ def run_game():
 	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 
-	ship = Ship(screen)
+	ship = Ship(ai_settings,screen)
 
 
 
 	bg_color = (230,230,230)
 
 	while True:
-		gf.check_events()
+		gf.check_events(ship)
+		ship.update()
 		gf.update_screen(ai_settings,screen,ship)
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
+		# for event in pygame.event.get():
+		# 	if event.type == pygame.QUIT:
+		# 		sys.exit()
+		# screen.fill(ai_settings.bg_color)
+		# ship.blitme()
 
 
 		pygame.display.flip()
