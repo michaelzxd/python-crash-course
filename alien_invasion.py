e892084b8f46f47956fe1893ffb2f4c1f67f1e92
Xiaodong <403533049@qq.com>
2020-06-12 21:48:47 +0800

    99 percent finished

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,12 +11,16 @@ from game_stats import GameStats
 from ship import Ship
 from alien import Alien
 import game_functions as gf
+from button import Button
 
 def run_game():
 	pygame.init()
 	ai_settings = Settings()
 	screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+	play_button = Button(ai_settings, screen, "Play")
+
+
 	stats = GameStats(ai_settings)
 	ship = Ship(ai_settings,screen)
 	bullets = Group()
@@ -26,12 +30,12 @@ def run_game():
 	alien = Alien(ai_settings, screen)
 
 	while True:
-		gf.check_events(ai_settings,screen,ship,bullets)
+		gf.check_events(ai_settings,screen,stats, play_button, ship,bullets)
 		if stats.game_active:
 		    ship.update()
 		    gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
 		    gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
-		gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+		gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
 		# for event in pygame.event.get():
 		# 	if event.type == pygame.QUIT:
 		# 		sys.exit()
